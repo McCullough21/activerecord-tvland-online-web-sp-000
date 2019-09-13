ef3d3474e58374ce512fa518406b555bb66364ec
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-13 20:37:55 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,13 @@ class Show < ActiveRecord::Base
   belongs_to :network
   has_many :characters
   has_many :actors, through: :characters
+
+  def actors_list
+    actors = []
+    self.actors do |actor|
+      actors << actor.full_name
+    end
+    actors
+
+  end
 end
