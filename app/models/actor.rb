98c6fab6f8947fc826a0160975fa9c84233490e8
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-13 20:21:09 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,10 +6,11 @@ class Actor < ActiveRecord::Base
     [self.first_name, self.last_name].join(" ")
   end
   def list_roles
-    self.characters.each do |character|
+    roles = self.characters.each do |character|
       puts [character.name, character.show.name].join(" - ")
       # binding.pry
     end
+    roles
   end
 
 end
